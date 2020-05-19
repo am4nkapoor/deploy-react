be0f3adedfaa51fca7935c82b8b5d95ee461b337
Aman Kapoor <aman.kapoor@collegedunia.com>
2020-05-19 15:46:43 +0530

    Updates

---
+++ precache-manifest.3d308e0b12d84b87b52b575f4feff446.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2a5ff0d44ed16f6caec62219f7abbcf",
+    "url": "/deploy-react/index.html"
+  },
+  {
+    "revision": "5cfb644898cc973ade84",
+    "url": "/deploy-react/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "fd6481eb5194d0fe544a",
+    "url": "/deploy-react/static/js/2.a95dc5dc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/deploy-react/static/js/2.a95dc5dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5cfb644898cc973ade84",
+    "url": "/deploy-react/static/js/main.dc243928.chunk.js"
+  },
+  {
+    "revision": "706a261ec706040237af",
+    "url": "/deploy-react/static/js/runtime-main.aade7fa5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/deploy-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
